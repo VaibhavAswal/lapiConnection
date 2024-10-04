08422615b684b0aacd5e465ca73f05d2d9d94fde
Vaibhav Aswal <vbhv.aswal@gmail.com>
2024-10-04 18:44:48 +0530

    0.9.0

--- models/chatModel.js
+++ models/chatModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
     ip: { type: String, required: true },
-    message: { type: String, required: true },
+    message: { type: Object, required: true },
     sender: { type: String, required: true },
     timestamp: { type: Date, default: Date.now },
     status: { type: String, default: 'processing' }
