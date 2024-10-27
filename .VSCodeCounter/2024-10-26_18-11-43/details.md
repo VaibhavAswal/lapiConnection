a98e6e0acdaaf83f2547e9f9a09dd836d0fd4e4f
Vaibhav Aswal <vbhv.aswal@gmail.com>
2024-10-27 10:39:13 +0530

    added delay

---
+++ .VSCodeCounter/2024-10-26_18-11-43/details.md
@@ -0,0 +1,38 @@
+# Details
+
+Date : 2024-10-26 18:11:43
+
+Directory g:\\lapiBackend
+
+Total : 23 files,  4606 codes, 68 comments, 133 blanks, all 4807 lines
+
+[Summary](results.md) / Details / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Files
+| filename | language | code | comment | blank | total |
+| :--- | :--- | ---: | ---: | ---: | ---: |
+| [controllers/authController.js](/controllers/authController.js) | JavaScript | 299 | 6 | 36 | 341 |
+| [controllers/dataController.js](/controllers/dataController.js) | JavaScript | 35 | 3 | 8 | 46 |
+| [controllers/lapiController.js](/controllers/lapiController.js) | JavaScript | 112 | 7 | 7 | 126 |
+| [docs/swagger.js](/docs/swagger.js) | JavaScript | 32 | 8 | 6 | 46 |
+| [docs/swagger.json](/docs/swagger.json) | JSON | 595 | 0 | 0 | 595 |
+| [emailTemplates/forgotPass.js](/emailTemplates/forgotPass.js) | JavaScript | 464 | 0 | 2 | 466 |
+| [emailTemplates/verify.html](/emailTemplates/verify.html) | HTML | 95 | 10 | 2 | 107 |
+| [emailTemplates/verifyMail.js](/emailTemplates/verifyMail.js) | JavaScript | 108 | 0 | 2 | 110 |
+| [middleware/adminVerify.js](/middleware/adminVerify.js) | JavaScript | 17 | 0 | 2 | 19 |
+| [middleware/authenticateToken.js](/middleware/authenticateToken.js) | JavaScript | 23 | 0 | 1 | 24 |
+| [middleware/errorHandler.js](/middleware/errorHandler.js) | JavaScript | 13 | 0 | 2 | 15 |
+| [models/chatModel.js](/models/chatModel.js) | JavaScript | 9 | 1 | 3 | 13 |
+| [models/nvrModel.js](/models/nvrModel.js) | JavaScript | 35 | 0 | 3 | 38 |
+| [models/userModel.js](/models/userModel.js) | JavaScript | 49 | 0 | 3 | 52 |
+| [package-lock.json](/package-lock.json) | JSON | 2,417 | 0 | 1 | 2,418 |
+| [package.json](/package.json) | JSON | 30 | 0 | 1 | 31 |
+| [routes/authRoute.js](/routes/authRoute.js) | JavaScript | 17 | 0 | 4 | 21 |
+| [routes/dataRoute.js](/routes/dataRoute.js) | JavaScript | 6 | 0 | 4 | 10 |
+| [routes/lapiRoute.js](/routes/lapiRoute.js) | JavaScript | 4 | 0 | 4 | 8 |
+| [server.js](/server.js) | JavaScript | 88 | 3 | 18 | 109 |
+| [services/websocketMessage.js](/services/websocketMessage.js) | JavaScript | 118 | 30 | 20 | 168 |
+| [utils/mailer.js](/utils/mailer.js) | JavaScript | 33 | 0 | 3 | 36 |
+| [utils/tryCatch.js](/utils/tryCatch.js) | JavaScript | 7 | 0 | 1 | 8 |
+
+[Summary](results.md) / Details / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- services/websocketMessage.js
+++ services/websocketMessage.js
@@ -11,10 +11,10 @@ let client;
 function updateRTSPUrl(responseObj, username, password, newIp) {
   // Extract the current RTSP URL from the response object
   const currentUrl = responseObj?.Data?.URL;
-  console.log("local ip url :" + currentUrl)
+  console.log("local ip url :" + currentUrl);
 
   // Find the part of the URL after the IP and port (after ":554")
-  const pathIndex = currentUrl?.indexOf(':554/');
+  const pathIndex = currentUrl?.indexOf(":554/");
   const path = currentUrl?.substring(pathIndex + 5); // Extract everything after ":554/"
 
   // Construct the new RTSP URL with username, password, and new IP
@@ -24,21 +24,22 @@ function updateRTSPUrl(responseObj, username, password, newIp) {
 }
 
 // Function to convert time to Unix timestamp with 6.5-hour delta
-function getUnixTimestamp(dateStr, timeStr, deltaHours = -5.5) {
+function getUnixTimestamp(dateStr, timeStr, delay = 0, deltaHours = -5.5) {
   const date = new Date(dateStr); // Parse the date
-  const [time, modifier] = timeStr.split(' '); // Split time and AM/PM
-  let [hours, minutes] = time.split(':').map(Number);
+  const [time, modifier] = timeStr.split(" "); // Split time and AM/PM
+  let [hours, minutes] = time.split(":").map(Number);
 
   // Adjust hours based on AM/PM
-  if (modifier === 'P.M' && hours !== 12) hours += 12;
-  if (modifier === 'A.M' && hours === 12) hours = 0;
+  if (modifier === "P.M" && hours !== 12) hours += 12;
+  if (modifier === "A.M" && hours === 12) hours = 0;
 
   // Set the hours and minutes in the date object
   date.setHours(hours, minutes, 0, 0);
 
   // Add or subtract the delta (in seconds)
   const deltaInSeconds = deltaHours * 3600; // Convert hours to seconds
-  const adjustedTimeInSeconds = Math.floor(date.getTime() / 1000) + deltaInSeconds;
+  const adjustedTimeInSeconds =
+    Math.floor(date.getTime() / 1000) + deltaInSeconds + delay;
 
   // Return the adjusted Unix timestamp in seconds
   return adjustedTimeInSeconds;
@@ -46,15 +47,22 @@ function getUnixTimestamp(dateStr, timeStr, deltaHours = -5.5) {
 
 exports.sendMsg = (msg, ip) => {
   const nvrWs = nvrConnections.get(ip);
-  console.log(msg)
+  console.log(msg);
   if (nvrWs) {
     // nvrWs.send(JSON.stringify(msg));
     console.log("sending request to nvr: " + ip);
-    const startTimestamp = getUnixTimestamp(msg.date, msg.time.start);
-    const endTimestamp = getUnixTimestamp(msg.date, msg.time.end);
+    const startTimestamp = getUnixTimestamp(
+      (dateStr = msg.date),
+      (timeStr = msg.time.start),
+      (delay = msg.delay)
+    );
+    const endTimestamp = getUnixTimestamp(
+      (dateStr = msg.date),
+      (timeStr = msg.time.end),
+      (delay = msg.delay)
+    );
     const formattedMsg = {
-      RequestURL:
-        `/LAPI/V1.0/Channels/${msg.channel}/Media/Video/Streams/RecordURL?Begin=${startTimestamp}&End=${endTimestamp}&Types=Normal&RelationOfTypes=AND&Position=Local&SessionID=123456&TransType=HTTP`,
+      RequestURL: `/LAPI/V1.0/Channels/${msg.channel}/Media/Video/Streams/RecordURL?Begin=${startTimestamp}&End=${endTimestamp}&Types=Normal&RelationOfTypes=AND&Position=Local&SessionID=123456&TransType=HTTP`,
       Method: "GET",
       Cseq: 1,
       Data: {},
@@ -118,7 +126,10 @@ exports.handleMessage = async (ws, req) => {
         // await chat.save();
         // client.send(JSON.stringify(message));
         // console.log("Message received:" + JSON.stringify(data));
-        console.log("Stream url :",updateRTSPUrl(data, "admin", "admin_123", nvr.Ip));
+        console.log(
+          "Stream url :",
+          updateRTSPUrl(data, "admin", "admin_123", nvr.Ip)
+        );
       }
     });
 
