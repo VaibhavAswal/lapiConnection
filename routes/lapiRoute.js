f405a8ef9f3a2e04544452000d3763d391b58c73
Vaibhav Aswal <vbhv.aswal@gmail.com>
2024-11-23 10:28:06 +0530

    change angle

--- routes/lapiRoute.js
+++ routes/lapiRoute.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
-const { register } = require("../controllers/lapiController");
+const { register, changeCameraAgl } = require("../controllers/lapiController");
 
 router.get("/System/UpServer/Register", register);
+router.post("/changeangle", changeCameraAgl);
 
 module.exports = router;
